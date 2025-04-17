039e20f1bb04cd4e43f4a9f1cc0360c842490808
liuk123456789 <18329177975@163.com>
2025-04-17 05:33:29 +0000

    feat: add memoize

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,1 +1,33 @@
 export { debounce, throttle } from 'throttle-debounce'
+
+export type ResolverFunction<TArgs extends unknown[], TKey> = (...args: TArgs) => TKey
+
+/**
+ * @description 缓存函数
+ * @param func { Function }
+ * @param resolver { Function }
+ * @returns memoized
+ */
+export function memoize<TArgs extends unknown[], TResult, TKey = string>(
+  func: (...args: TArgs) => TResult,
+  resolver?: ResolverFunction<TArgs, TKey>,
+): {
+    (...args: TArgs): TResult
+    cache: Map<TKey, TResult>
+  } {
+  const memoized = function (this: any, ...args: TArgs): TResult {
+    const key = resolver ? resolver.apply(this, args) : JSON.stringify(args) as TKey
+
+    if (memoized.cache.has(key)) {
+      return memoized.cache.get(key)!
+    }
+
+    const result = func.apply(this, args)
+    memoized.cache.set(key, result)
+    return result
+  }
+
+  memoized.cache = new Map<TKey, TResult>()
+
+  return memoized
+}
